0853d9f0e1aa6a80cbd2fced835095caa1e83d90
Lei da sorte <75717568+Frederick528@users.noreply.github.com>
2026-01-14 13:47:42 +0900

    Book CRUD 제작
    일단 List로 받아오고 있지만, 나중에 Page로 받을 듯.

--- backend-api/src/main/java/com/ohgiraffers/backendapi/domain/book/controller/BookController.java
+++ backend-api/src/main/java/com/ohgiraffers/backendapi/domain/book/controller/BookController.java
@@ -1,4 +1,39 @@
 package com.ohgiraffers.backendapi.domain.book.controller;
 
+import com.ohgiraffers.backendapi.domain.book.dto.BookRequestDTO;
+import com.ohgiraffers.backendapi.domain.book.dto.BookResponseDTO;
+import com.ohgiraffers.backendapi.domain.book.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Long> createBook(@RequestBody BookRequestDTO request) {
+        return ResponseEntity.ok(bookService.createBook(request));
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<BookResponseDTO> getBook(@PathVariable Long bookId) {
+        return ResponseEntity.ok(bookService.getBook(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookResponseDTO>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @DeleteMapping("/{bookId}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long bookId) {
+        bookService.deleteBook(bookId);
+        return ResponseEntity.noContent().build();
+    }
 }
