0ad5caae21bfcef19440780a2d878f16eaef332f
Kimhyungyun812 <hyungyun812@gmail.com>
2026-01-14 11:20:46 +0900

    community 추가
    community 추가

--- backend-api/src/main/java/com/ohgiraffers/backendapi/domain/community_comments/service/CommunityCommentService.java
+++ backend-api/src/main/java/com/ohgiraffers/backendapi/domain/community_comments/service/CommunityCommentService.java
@@ -1,5 +1,9 @@
 package com.ohgiraffers.backendapi.domain.community_comments.service;
 
+<<<<<<< Updated upstream
+=======
+import com.ohgiraffers.backendapi.domain.community_comments.dto.CommunityCommentRequest;
+>>>>>>> Stashed changes
 import com.ohgiraffers.backendapi.domain.community_comments.entity.CommunityComment;
 import com.ohgiraffers.backendapi.domain.community_comments.repository.CommunityCommentRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommunityCommentService {
 
+<<<<<<< Updated upstream
     private final CommunityCommentRepository communityCommentRepository;
 
     public CommunityComment create(CommunityComment comment) {
@@ -19,5 +24,38 @@ public class CommunityCommentService {
 
     public List<CommunityComment> findByPostId(Long postId) {
         return communityCommentRepository.findByPostId(postId);
+=======
+    private final CommunityCommentRepository repository;
+
+    public CommunityComment create(
+            CommunityCommentRequest.Create request,
+            Long postId,
+            Long userId
+    ) {
+        return repository.save(
+                CommunityComment.builder()
+                        .content(request.getContent())
+                        .parentId(request.getParentId())
+                        .postId(postId)
+                        .userId(userId)
+                        .build()
+        );
+    }
+
+    public List<CommunityComment> findByPostId(Long postId) {
+        return repository.findByPostId(postId);
+    }
+
+    public CommunityComment update(Long commentId, CommunityCommentRequest.Update request) {
+        CommunityComment comment = repository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 없음"));
+
+        comment.update(request.getContent());
+        return repository.save(comment);
+    }
+
+    public void delete(Long commentId) {
+        repository.deleteById(commentId);
+>>>>>>> Stashed changes
     }
 }
