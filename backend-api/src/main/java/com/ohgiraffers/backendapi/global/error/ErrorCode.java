4c55b16fbbaabfd08ff1c56c9b954d28143c5665
HOT8OY <4321qwaszx@naver.com>
2026-01-15 23:20:54 +0900

    ErrorCode 생성자 \@RequiredArgsConstructor으로 대체

--- backend-api/src/main/java/com/ohgiraffers/backendapi/global/error/ErrorCode.java
+++ backend-api/src/main/java/com/ohgiraffers/backendapi/global/error/ErrorCode.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.backendapi.global.error;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 오류가 발생했습니다."),
     CANNOT_REQUEST_TO_SELF(HttpStatus.BAD_REQUEST, "F001", "자기 자신에게는 친구 요청을 보낼 수 없습니다."),
@@ -15,11 +17,4 @@ public enum ErrorCode {
     private final HttpStatus status;
     private final String code;
     private final String message;
-
-    ErrorCode(HttpStatus status, String code, String message) {
-        this.status = status;
-        this.code = code;
-        this.message = message;
-    }
-
 }
