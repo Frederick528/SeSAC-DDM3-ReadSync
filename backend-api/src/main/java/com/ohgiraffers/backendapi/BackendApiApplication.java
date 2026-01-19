2b2833f552d8a9e8658c4361b1b555733d544554
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2026-01-19 15:25:24 +0900

    #28/feat: Readingroom 관련 repository 생성, main 코드 위 @EnableJpaAuditing 추가

--- backend-api/src/main/java/com/ohgiraffers/backendapi/BackendApiApplication.java
+++ backend-api/src/main/java/com/ohgiraffers/backendapi/BackendApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// TODO: (요청) '@EnableJpaAuditing' 붙이기 - AuditingEntityListener 기능 사용으로 인해 요청
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApiApplication {
 
--- backend-api/src/main/java/com/ohgiraffers/backendapi/domain/readingroom/repository/ReadingRoomRepository.java
+++ backend-api/src/main/java/com/ohgiraffers/backendapi/domain/readingroom/repository/ReadingRoomRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 public interface ReadingRoomRepository extends JpaRepository<ReadingRoom, Long> {
 
     // 방장이 현재 운영 중인 방이 있는지 확인 (중복 방 생성 방지용)
-    Optional<ReadingRoom> findByHost_UserIdAndStatusNot(Long hostId, RoomStatus status);
+    Optional<ReadingRoom> findByHost_IdAndStatusNot(Long hostId, RoomStatus status);
 }
