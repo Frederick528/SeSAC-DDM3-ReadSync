77ec0b9aed44a54ff88e8f646d5ff51a5788b4dc
jaeseung9 <seojaeseung9@gmail.com>
2026-01-22 09:19:25 +0900

    도메인주소 변경

--- backend-api/src/main/java/com/ohgiraffers/backendapi/domain/user/service/AuthService.java
+++ backend-api/src/main/java/com/ohgiraffers/backendapi/domain/user/service/AuthService.java
@@ -17,7 +17,7 @@ import com.ohgiraffers.backendapi.global.error.CustomException;
 import com.ohgiraffers.backendapi.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate; // ★ Redis 추가
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
